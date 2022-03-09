2d72a7c5471bbd625cbb7bf3e952b9eb9d63a4a4
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-03-09 12:23:59 +0530

    Basic html layout and styles configured

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+		],
+	},
+	devServer: {
+		static: path.join(__dirname, "dist"),
+		port: 3000,
+		open: true,
+		hot: true,
+	},
+};
